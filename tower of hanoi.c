c7ca0a97964302c92ced1fd31ce46beffbc10ba3
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-12-13 21:48:00 +0530

    Create tower of hanoi.c

---
+++ tower of hanoi.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void tower(int,char,char,char);
+int main()
+{
+	int n;
+	printf("enter no of disks:");
+	scanf("%d",&n);
+		if(n==0)
+		{
+			printf("no disk found\n");
+			return 0;
+		}		
+	printf("moves involed in tower of hanio\n");
+	tower(n,'A','C','B');
+return 0;
+}
+
+void tower(intn,charsource,chardest,char temp)
+{
+	if(n==1)
+	{
+		printf("move %d disk from %c to %c\n",n,source,dest);
+		return;
+	}
+	tower(n-1,source,temp,dest);
+	printf("move %d disk from %c to %c\n",n,source,dest);
+	tower(n-1,temp,dest,source);
+}
+
+
